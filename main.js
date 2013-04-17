1a230ae2a07b4464501cb4c4e91bee85e23fba6f
nighca <nighca@live.cn>
2013-04-17 22:36:32 +0800

    add infoMgr

--- main.js
+++ main.js
@@ -1,5 +1,39 @@
 if(!myEx){
-	var myEx = function(){
+	var useMyEx = function(){
+		var genInfoMgr = function(){
+			var infoBox = $('<div id="myEx-info-box"></div>');
+			infoBox.css({
+				position: "absolute",
+				top: 0,
+				left: 0,
+				width: "100%",
+				height: "100%",
+				fontWeight: "bold",
+				color: "rgba(30, 30, 30, 0.8)",
+				backgroundColor: "rgba(250, 250, 250, 0.5)",
+				fontSize: "60px",
+				textAlign: "center",
+
+				display: "none",
+				zIndex: 200
+			}).appendTo("body").css({
+				lineHeight: infoBox.height() + "px"
+			});
+
+			var showInfo = function(info, delay){
+				delay = delay || 500;
+				if (["string", "number"].indexOf(typeof(info)) < 0) {
+					info = JSON.stringify(info);
+				};
+				infoBox.text(info).fadeIn().delay(delay).fadeOut();
+			};
+
+			return {
+				showInfo : showInfo
+			};
+		};
+		var infoMgr = genInfoMgr();
+
 		var removeAD = function(){
 			$(".ecom-ads-wrap").hide();
 			$(".ui-reelList-viewport").css("bottom", "0");
@@ -34,7 +68,6 @@ if(!myEx){
 					ctlShortCuts[cmd] && ctlShortCuts[cmd]();
 				}
 			};
-
 
 			return {
 				enable: function(){
@@ -108,14 +141,17 @@ if(!myEx){
 				// List - 'L'
 				"76": function(event){
 					$("#playMode").find(".list-mode > a").click();
+					infoMgr.showInfo("List");
 				},
 				// Random - 'R'
 				"82": function(event){
 					$("#playMode").find(".random-mode > a").click();
+					infoMgr.showInfo("Random");
 				},
 				// Single - 'S'
 				"83": function(event){
 					$("#playMode").find(".single-mode > a").click();
+					infoMgr.showInfo("Single");
 				},
 				// LRC FullScreen - '+'
 				"187": function(event){
@@ -133,9 +169,13 @@ if(!myEx){
 				}
 			}
 		}).enable();
+
+		return {
+			showInfo: infoMgr.showInfo
+		};
 	};
-	myEx();
-	console.log("myEx ok.");
+	var myEx = useMyEx();
+	myEx.showInfo("myEx ok.");
 }else{
-	console.log("myEx already running.")
+	myEx.showInfo("myEx already running.")
 }
